2fe2f585185b4a1e5dc2d94d7ea7c796436cfe92
gerry-dns <germain.denis@yahoo.fr>
2023-01-12 16:23:48 +0100

    :speech_balloon: formulaire de contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Form\ContactType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact.index')]
+    public function index(EntityManagerInterface $manager, Request $request): Response
+    {
+        $contact = new Contact();
+
+        // if there is a current user
+        if($this->getUser()) {
+            // FullName and Email will be already filled-in
+            $contact->setFullName($this->getUser()->getFullName())
+                ->setEmail($this->getUser()->getEmail());
+        }
+
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+
+            $manager->persist($contact);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre message a bien été envoyé'
+            );
+            
+            return $this->redirectToRoute('contact.index');
+        }
+
+        return $this->render('pages/contact/index.html.twig', [
+            'form' => $form->createView(),
+            
+        ]);
+    }
+}
