a5a0e9e50f28a51f9a5343f1a89c90627ff8ac09
gerry-dns <germain.denis@yahoo.fr>
2023-01-13 17:54:59 +0100

    :closed_lock_with_key: mis en place du recaptcha google dans le form de contact

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Karser\Recaptcha3Bundle\Form\Recaptcha3Type;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -11,6 +12,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Karser\Recaptcha3Bundle\Validator\Constraints\Recaptcha3;
 
 class ContactType extends AbstractType
 {
@@ -76,8 +78,12 @@ class ContactType extends AbstractType
                     'class' => 'btn btn-primary mt-3'
                 ],
                 'label' => 'Envoyer'
-            ]); 
-        ;
+            ])
+            ->add('captcha', Recaptcha3Type::class, [
+                'constraints' => new Recaptcha3(),
+                'action_name' => 'contact',
+                'locale' => 'de',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
